58e0538148894f57ffa9d7117ffbb5f7ce307724
susu <u1240976@gmail.com>
2015-01-17 18:47:07 +0800

    add fish config files

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,27 @@
+### fish color ###
+# fish_color_autosuggestion '666'  'green'
+# fish_color_command afffff
+# fish_color_comment red
+# fish_color_cwd green
+# fish_color_cwd_root red
+# fish_color_error 'red'  '--bold'
+# fish_color_escape cyan
+# fish_color_history_current cyan
+# fish_color_host '-o'  'cyan'
+# fish_color_match cyan
+# fish_color_normal normal
+# fish_color_operator cyan
+# fish_color_param '00afff'  'cyan'
+# fish_color_quote brown
+# fish_color_redirection normal
+# fish_color_search_match --background=purple
+# fish_color_status red
+# fish_color_user '-o'  'green'
+# fish_color_valid_path --underline
+
+set -g fish_color_host cyan -o
+set -g fish_color_user green -o
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_status red
+
