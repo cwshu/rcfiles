af9d3c2a5e9b5e735599839a65de98083632f15a
susu <u1240976@gmail.com>
2015-02-26 19:58:33 +0800

    test setup rc files script

---
+++ setup_rc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import subprocess as sp
+import os
+
+rc_dir = os.path.dirname(os.path.abspath(__file__))
+home_dir = os.environ['HOME']
+
+print "ln -s {rc_dir}/home/.* {home}/".format(rc_dir=rc_dir, home=home_dir)
+print "ln -s {rc_dir}/.config/fish {home}/.config/fish".format(rc_dir=rc_dir, home=home_dir)
+# sp.call("ln -s {rc_dir}/home/.* {home}/".format(rc_dir=rc_dir, home=home_dir), shell=True)
+# sp.call("ln -s {rc_dir}/.config/fish {home}/.config/fish".format(rc_dir=rc_dir, home=home_dir), shell=True)
+
