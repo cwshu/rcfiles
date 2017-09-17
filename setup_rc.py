c7fce52fbdd398518e0e50527ed9f49a76f8e085
susu <u1240976@gmail.com>
2017-09-18 01:46:41 +0800

    fish/dot_config: modify directory tree

--- setup_rc.py
+++ setup_rc.py
@@ -8,18 +8,23 @@ rc = os.path.dirname(os.path.abspath(__file__))
 home = os.environ['HOME']
 
 if os.environ.has_key('XDG_CONFIG_HOME'):
-    xdg_config_home = os.environ['XDG_CONFIG_HOME']
+    dot_config = os.environ['XDG_CONFIG_HOME']
 else:
-    xdg_config_home = home + '/.config'
+    dot_config = home + '/.config'
 
 # do soft link for each config
 def main():
     print "ln -s {rc}/home/.* {home}/".format(rc=rc, home=home)
-    print "ln -s {rc}/.config/fish {xdg_config_home}/fish".format(rc=rc, xdg_config_home=xdg_config_home)
 
+    # .config/ and fish
+    print "mkdir -p {dot_config}/".format(dot_config=dot_config)
+    print "mv {dot_config}/fish {dot_config}/fish.old".format(dot_config=dot_config)
+    print "ln -s {rc}/dot_config/fish {dot_config}/fish".format(rc=rc, dot_config=dot_config)
+
+    # vim
     print "ln -s {rc}/vim {home}/.vim".format(rc=rc, home=home)
     print "ln -s {rc}/vim/init.vim {home}/.vimrc".format(rc=rc, home=home)
-    print "ln -s {rc}/vim {xdg_config_home}/nvim".format(rc=rc, xdg_config_home=xdg_config_home)
+    print "ln -s {rc}/vim {dot_config}/nvim".format(rc=rc, dot_config=dot_config)
 
 if __name__ == '__main__':
     main()
