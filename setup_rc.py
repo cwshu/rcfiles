298fa4180332033631a4398748b023f1a2cffad9
Jim Shu <cwshu09@gmail.com>
2021-12-12 23:35:41 +0800

    Update setup script to Python3
    Also keep Python2.7 compatibility by __future__ module

--- setup_rc.py
+++ setup_rc.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function
 import subprocess as sp
 import os
 
@@ -7,24 +7,24 @@ import os
 rc = os.path.dirname(os.path.abspath(__file__))
 home = os.environ['HOME']
 
-if os.environ.has_key('XDG_CONFIG_HOME'):
+if 'XDG_CONFIG_HOME' in os.environ.keys():
     dot_config = os.environ['XDG_CONFIG_HOME']
 else:
     dot_config = home + '/.config'
 
 # do soft link for each config
 def main():
-    print "ln -s {rc}/home/.* {home}/".format(rc=rc, home=home)
+    print("ln -s {rc}/home/.* {home}/".format(rc=rc, home=home))
 
     # .config/ and fish
-    print "mkdir -p {dot_config}/".format(dot_config=dot_config)
-    print "mv {dot_config}/fish {dot_config}/fish.old".format(dot_config=dot_config)
-    print "ln -s {rc}/dot_config/fish {dot_config}/fish".format(rc=rc, dot_config=dot_config)
+    print("mkdir -p {dot_config}/".format(dot_config=dot_config))
+    print("mv {dot_config}/fish {dot_config}/fish.old".format(dot_config=dot_config))
+    print("ln -s {rc}/dot_config/fish {dot_config}/fish".format(rc=rc, dot_config=dot_config))
 
     # vim
-    print "ln -s {rc}/vim {home}/.vim".format(rc=rc, home=home)
-    print "ln -s {rc}/vim/init.vim {home}/.vimrc".format(rc=rc, home=home)
-    print "ln -s {rc}/vim {dot_config}/nvim".format(rc=rc, dot_config=dot_config)
+    print("ln -s {rc}/vim {home}/.vim".format(rc=rc, home=home))
+    print("ln -s {rc}/vim/init.vim {home}/.vimrc".format(rc=rc, home=home))
+    print("ln -s {rc}/vim {dot_config}/nvim".format(rc=rc, dot_config=dot_config))
 
 if __name__ == '__main__':
     main()
